a95ec6e454ad726e0a45f047d010835c9f4f1704
Ashfak Niloy <ashfakniloy56@gmail.com>
2021-09-13 21:39:58 +0600

    Hide about link in about page

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "@/styles/Footer.module.css";
 
 export default function Footer() {
+  const router = useRouter();
+
   return (
     <footer className={styles.footer}>
       <p>Copyright &copy; DJ events 2021</p>
-      <p>
-        <Link href="/about">About this project</Link>
-      </p>
+
+      {router.pathname !== "/about" && (
+        <p>
+          <Link href="/about">About this project</Link>
+        </p>
+      )}
     </footer>
   );
 }
